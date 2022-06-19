ea2dc286d597fdfdf2d13e23b3f9259b2558b2ee
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-06-19 09:44:51 +0530

    Delete configs.py

--- configs.py
+++
@@ -1,9 +0,0 @@
-# Copyright ©️ 2022 Sanila Ranatunga. All Rights Reserved
-
-import os
-
-
-class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")

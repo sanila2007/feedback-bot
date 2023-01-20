d09364db7f3e64481d66b25137c871dc8041a551
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2023-01-20 17:23:54 +0530

    Add files via upload

---
+++ util/date_info.py
@@ -0,0 +1,37 @@
+# Copyright ©️ 2022 Sanila Ranatunga. All Rights Reserved
+# You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [feedback-bot] (https://github.com/sanila2007/feedback-bot)
+
+# Read GNU General Public License v3.0: https://github.com/sanila2007/feedback-bot/blob/mai/LICENSE
+# Don't forget to follow github.com/sanila2007 because I'm doing these things for free and open source
+# Star and fork and enjoy!
+
+
+import datetime
+from datetime import timedelta
+
+# %a = weekday(short)
+# %A = weekday(full)
+# %b = month name(short)
+# %B = month name(full)
+# %y = year(short)
+# %Y = year(full)
+# %d = day
+# %H = hour(00-23)
+# %I = hour(00-12)
+# %S = second
+# %f = microsecond
+# %p = AM/PM
+
+
+POSTED_DATE = datetime.date.today()
+
+time = datetime.datetime.now()
+
+POSTED_TIME = time.strftime("%I.%M %p")
+
+replydate = datetime.datetime.now() + timedelta(days=2)
+
+DATE_OF_REPLY = replydate.strftime("%Y-%m-%d")
+
+CAPTCHA_DATE = datetime.datetime.now() + timedelta(seconds=1)
